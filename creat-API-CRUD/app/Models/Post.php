0b10f892fe41c04cd37fa97ec00716a19b55da93
chaima marzouk <marzouk.chaima.892@gmail.com>
2021-07-27 15:07:01 +0100

    creat fillable in Model

--- creat-API-CRUD/app/Models/Post.php
+++ creat-API-CRUD/app/Models/Post.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $table = "series";
+
+    protected $fillable = ['titre','description','contenu'];
 }
