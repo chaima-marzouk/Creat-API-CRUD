f0d34f2378ceae6c76885d94f6ceaecd269c44e8
chaima marzouk <marzouk.chaima.892@gmail.com>
2021-07-27 16:46:56 +0100

    ajout de Controler/model/seed/factory of the new database

---
+++ creat-API-CRUD/database/factories/BookFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\book;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class BookFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = book::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
